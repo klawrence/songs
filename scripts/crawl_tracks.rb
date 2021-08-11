2879596d243660e23ea1c9673735ddf7322d83ac
kevin <kevin@diamond-sky.com>
2021-08-11 18:37:59 +0100

    Remove crawler scripts

--- scripts/crawl_tracks.rb
+++
@@ -1,9 +0,0 @@
-crawler = ChartCrawler.new 'https://www.uk-charts.top-source.info'
-
-crawler.crawl_chart '1950s', '/1952-to-1959.shtml'
-
-year = 1960
-while year < 2030
-  crawler.crawl_chart "#{year}s", "/#{year}-to-#{year + 9}.shtml"
-  year += 10
-end
