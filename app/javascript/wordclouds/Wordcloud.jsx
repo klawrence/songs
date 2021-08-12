0b59e94dc4c70a5ca7dcc7719aeaebe9d28db7fe
kevin <kevin@diamond-sky.com>
2021-08-12 15:58:03 +0100

    Create a wordcloud

--- app/javascript/wordclouds/Wordcloud.jsx
+++ app/javascript/wordclouds/Wordcloud.jsx
@@ -1,5 +1,27 @@
 import React from 'react'
+import ReactWordcloud from 'react-wordcloud'
+
+const words = [
+  {
+    text: 'told',
+    value: 20,
+  },
+  {
+    text: 'mistake',
+    value: 22,
+  },
+  {
+    text: 'thought',
+    value: 16,
+  },
+  {
+    text: 'bad',
+    value: 17,
+  },
+]
 
 export function Wordcloud({url}) {
-  return <div className='wordcloud'>{url}</div>
+  return <div className='wordcloud'>
+    <ReactWordcloud words={words} />
+  </div>
 }
